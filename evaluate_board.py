74b313f0681251d35585147db1595e154fca48ff
lookicode <lukasjanecek13@gmail.com>
2025-08-08 23:22:59 +0200

    evaluate_board: make PIECE_VALUES global

--- evaluate_board.py
+++ evaluate_board.py
@@ -1,5 +1,15 @@
 import chess
 
+PIECE_VALUES = {
+    chess.PAWN: 100,
+    chess.KNIGHT: 320,
+    chess.BISHOP: 330,
+    chess.ROOK: 500,
+    chess.QUEEN: 900,
+    chess.KING: 0,
+}
+
+
 def evaluate_board(board: chess.Board) -> int:
     """Positive scores favor White, negative favor Black."""
     if board.is_checkmate():
@@ -7,17 +17,9 @@ def evaluate_board(board: chess.Board) -> int:
     if board.is_stalemate() or board.is_insufficient_material():
         return 0
 
-    piece_values = {
-        chess.PAWN: 100,
-        chess.KNIGHT: 320,
-        chess.BISHOP: 330,
-        chess.ROOK: 500,
-        chess.QUEEN: 900,
-        chess.KING: 0,
-    }
 
     score = 0
-    for piece_type, value in piece_values.items():
+    for piece_type, value in PIECE_VALUES.items():
         score += len(board.pieces(piece_type, chess.WHITE)) * value
         score -= len(board.pieces(piece_type, chess.BLACK)) * value
 
